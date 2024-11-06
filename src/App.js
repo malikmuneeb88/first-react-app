fec4d97393014a582aaf458f010e2e2b12ea3187
Vyro <mmm906191@gmail.com>
2024-11-06 15:25:12 +0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com?apikey=18bb89c2';
+
+const movie1 = {
+    "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+    "Year": "2007",
+    "imdbID": "tt1132238",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
+} 
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input
+                placeholder="Search for Movies"
+                value="Superman"
+                onChange={() => {}}
+            />
+            <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+            />
+        </div>
+
+        <div className="container">
+            <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+
+                <div>
+                    <img src={movie1.Poster !== 'N/A' ? movie1.Poster : } />
+                </div>
+            </div>
+        </div>
     </div>
+    
   );
-}
+};
 
 export default App;
