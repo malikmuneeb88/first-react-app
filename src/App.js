79e00b9cbd05ea448bd2801ff2d96d9066a10571
Vyro <mmm906191@gmail.com>
2024-11-07 13:42:05 +0500

    first react app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import MovieCard from './MovieCard';
 
@@ -17,12 +17,14 @@ const movie1 = {
 } 
 
 const App = () => {
+    const [movies, setMovies] = useState();
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -36,19 +38,29 @@ const App = () => {
         <div className="search">
             <input
                 placeholder="Search for Movies"
-                value="Superman"
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
             <img
                 src={SearchIcon}
                 alt="search"
-                onClick={() => {}}
+                onClick={() => searchMovies(searchTerm)}
             />
         </div>
 
-        <div className="container">
-            <MovieCard movie={movie1} />
-        </div>
+        {movies?.length > 0
+            ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                    </div>
+            ) : (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+                )}
+
     </div>
     
   );
